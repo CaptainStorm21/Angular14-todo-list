66032f5928c7eb19269736b33da8359a05cfebf4
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-11-13 23:56:11 -0800

    25. Style angular material calendar

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -1,15 +1,20 @@
+import { MatDatepickerControl } from './../../../node_modules/@angular/material/datepicker/datepicker-base.d';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarComponent } from './calendar.component';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     CalendarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
     CalendarComponent
